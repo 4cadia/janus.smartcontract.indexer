1c83aa5f191497904db1df014a0ed9060c66f505
bruna@janusproj.com <bruna@janusproj.com>
2019-04-29 17:22:06 -0300

    code review of the addWebSite function

--- contracts/Indexer.sol
+++ contracts/Indexer.sol
@@ -33,23 +33,65 @@ contract Indexer{
     function getWebSite(string memory _tag) 
         public 
         view 
-        returns(string[] memory, string[] memory, string[] memory, string[] memory){
-        
-        string[] memory storageHashs;
-        string[] memory tags;
-        string[] memory titles;
-        string[] memory descriptions;
+        returns(string memory){
+      
+            WebSite[] storage webSites = websites[_tag];
+            
+            string memory result;
 
-        WebSite[] storage webSites = websites[_tag];
-        
-        for(uint i = 0; i < webSites.length; i++){
-            WebSite storage site = webSites[i];
-            storageHashs[i] = site.storageHash;
-            tags[i] = site.tag;
-            titles[i] = site.title;
-            descriptions[i] = site.description;
-        }
+            for(uint i = 0; i < webSites.length; i++){
+                WebSite storage site = webSites[i];
 
-        return (storageHashs, tags, titles, descriptions);
+                if (i == 0) {
+                    string memory resultConcat = strConcat("", site.storageHash, site.tag, site.title, site.description);  
+                    uint resultLenght = getStringLength(resultConcat);
+                    string memory resultSlice = getSlice(2, resultLenght, resultConcat);
+                    result = resultSlice;
+                } else {
+                    uint resultLenght = getStringLength(result);
+                    string memory resultSlice = getSlice(1, resultLenght, result);
+                    result = strConcat(resultSlice, site.storageHash, site.tag, site.title, site.description);   
+                }            
+            }
+
+            return (result);
+    }   
+  
+    function strConcat(string memory _a, string memory _b, string memory _c, string memory _d, string memory _e) private pure returns (string memory){
+        bytes memory _ba = bytes(_a);
+        bytes memory _bb = bytes(_b);
+        bytes memory _bc = bytes(_c);
+        bytes memory _bd = bytes(_d);
+        bytes memory _be = bytes(_e);
+        bytes memory _v = bytes(",");
+
+        string memory abcd = new string(_ba.length + _v.length + _bb.length + _v.length + _bc.length  + _v.length + _bd.length + _v.length + _be.length);   
+
+        bytes memory babcd = bytes(abcd);
+        uint k = 0;
+        for (uint i = 0; i < _ba.length; i++) babcd[k++] = _ba[i];   
+        babcd[k++] = _v[0];
+        for (uint i = 0; i < _bb.length; i++) babcd[k++] = _bb[i];
+        babcd[k++] = _v[0];
+        for (uint i = 0; i < _bc.length; i++) babcd[k++] = _bc[i];
+        babcd[k++] = _v[0];
+        for (uint i = 0; i < _bd.length; i++) babcd[k++] = _bd[i];
+        babcd[k++] = _v[0];
+        for (uint i = 0; i < _be.length; i++) babcd[k++] = _be[i];        
+        return string(babcd);
+    }    
+    
+     function getStringLength(string memory _text) private pure returns(uint){
+        bytes memory btext = bytes(_text);
+        return btext.length;
     }
+
+    
+    function getSlice(uint256 _begin, uint256 _end, string memory _text) private pure returns (string memory) {
+        bytes memory a = new bytes(_end-_begin+1);
+        for(uint i=0;i<=_end-_begin;i++){
+            a[i] = bytes(_text)[i+_begin-1];
+        }
+        return string(a);    
+    }   
 }

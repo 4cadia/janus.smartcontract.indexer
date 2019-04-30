c4d82f0619aa04708d2ad5d8cf1b7decf97e6553
Rodrigo Oliveira <rodrigo@janusproj.com>
2019-04-30 17:08:16 -0300

    Changed getWebSite return to string

--- contracts/Indexer.sol
+++ contracts/Indexer.sol
@@ -45,7 +45,6 @@ contract Indexer{
         view 
         returns(string memory){
       
-<<<<<<< HEAD
         string memory result;
 
         for(uint a = 0; a < _tags.length; a++){
@@ -53,62 +52,21 @@ contract Indexer{
             uint[] memory index = tagToIndex[_tags[a]];
 
             for(uint i = 0; i < index.length; i++){
-                 result = concat(result, websites[i].storageHash, websites[i].title, websites[i].description); 
+                 result = concat(result, websites[index[i]].storageHash, websites[index[i]].title, websites[index[i]].description); 
             }
         }
 
         return result;
     }   
 
-    function getByHash(string memory storageHash) public view returns (string memory) {
-        uint index = hashToIndex[storageHash];
-        Website memory website = websites[index];
-        return website.title;
-    }
-
-    function getByIndex(uint index) public view returns (string memory) {
-        Website memory website = websites[index];
-        return website.title;
-    }
-
-   function concat(string memory _a, string memory _b, string memory _c, string memory _d) public pure returns (string memory){
-=======
-            WebSite[] storage webSites = websites[_tag];
-            
-            string memory result;
-
-            for(uint i = 0; i < webSites.length; i++){
-                WebSite storage site = webSites[i];
-
-                if (i == 0) {
-                    string memory resultConcat = strConcat("", site.storageHash, site.tag, site.title, site.description);  
-                    uint resultLenght = getStringLength(resultConcat);
-                    string memory resultSlice = getSlice(2, resultLenght, resultConcat);
-                    result = resultSlice;
-                } else {
-                    uint resultLenght = getStringLength(result);
-                    string memory resultSlice = getSlice(1, resultLenght, result);
-                    result = strConcat(resultSlice, site.storageHash, site.tag, site.title, site.description);   
-                }            
-            }
-
-            return (result);
-    }   
-  
-    function strConcat(string memory _a, string memory _b, string memory _c, string memory _d, string memory _e) private pure returns (string memory){
->>>>>>> f7b9704d579debc7fe1b10821676e2cdf01e4487
+   function concat(string memory _a, string memory _b, string memory _c, string memory _d) private pure returns (string memory){
         bytes memory _ba = bytes(_a);
         bytes memory _bb = bytes(_b);
         bytes memory _bc = bytes(_c);
         bytes memory _bd = bytes(_d);
-        bytes memory _be = bytes(_e);
-        bytes memory _v = bytes(",");
+        bytes memory _v = bytes(";");
 
-<<<<<<< HEAD
-        string memory abcd = new string(_ba.length + _v.length + _bb.length + _v.length + _bc.length  + _v.length + _bd.length);   
-=======
-        string memory abcd = new string(_ba.length + _v.length + _bb.length + _v.length + _bc.length  + _v.length + _bd.length + _v.length + _be.length);   
->>>>>>> f7b9704d579debc7fe1b10821676e2cdf01e4487
+        string memory abcd = new string(_ba.length + _v.length + _bb.length + _v.length + _bc.length + _v.length + _bd.length);   
 
         bytes memory babcd = bytes(abcd);
         uint k = 0;
@@ -116,29 +74,9 @@ contract Indexer{
         babcd[k++] = _v[0];
         for (uint i = 0; i < _bb.length; i++) babcd[k++] = _bb[i];
         babcd[k++] = _v[0];
-        for (uint i = 0; i < _bc.length; i++) babcd[k++] = _bc[i];
-        babcd[k++] = _v[0];
-<<<<<<< HEAD
-        for (uint i = 0; i < _bd.length; i++) babcd[k++] = _bd[i];      
-=======
-        for (uint i = 0; i < _bd.length; i++) babcd[k++] = _bd[i];
-        babcd[k++] = _v[0];
-        for (uint i = 0; i < _be.length; i++) babcd[k++] = _be[i];        
->>>>>>> f7b9704d579debc7fe1b10821676e2cdf01e4487
+        for (uint i = 0; i < _bc.length; i++) babcd[k++] = _bc[i]; 
+         babcd[k++] = _v[0];
+        for (uint i = 0; i < _bd.length; i++) babcd[k++] = _bd[i];  
         return string(babcd);
     }    
-    
-     function getStringLength(string memory _text) private pure returns(uint){
-        bytes memory btext = bytes(_text);
-        return btext.length;
-    }
-
-    
-    function getSlice(uint256 _begin, uint256 _end, string memory _text) private pure returns (string memory) {
-        bytes memory a = new bytes(_end-_begin+1);
-        for(uint i=0;i<=_end-_begin;i++){
-            a[i] = bytes(_text)[i+_begin-1];
-        }
-        return string(a);    
-    }   
 }

82442be2735936c01fc2df4d44e758ef9a65b44a
Rodrigo Oliveira <rodrigo@janusproj.com>
2019-04-30 16:39:51 -0300

    Change the index contract

--- contracts/Indexer.sol
+++ contracts/Indexer.sol
@@ -1,55 +1,84 @@
-pragma solidity >=0.4.21 < 0.6.0;
+pragma solidity >= 0.4.21 < 0.6.0;
 pragma experimental ABIEncoderV2;
 
 contract Indexer{
 
-    struct WebSite{
+    struct Website{
         string storageHash;
-        string tag;
         string title;
         string description;
+        address owner;
     }
+    
+    Website[] private websites;
+    
+    mapping(string => uint) hashToIndex;
+    
+    mapping(string => uint[]) tagToIndex;
 
-    mapping(string => WebSite[]) websites;
-
-    event addWebSiteEvent(string _tag);
+    event addWebSiteEvent(string[] _tags);
 
     function addWebSite (
         string memory _storageHash, 
-        string memory _tag, 
+        string[] memory _tags, 
         string memory _title, 
         string memory _description) public {
 
-        websites[_tag].push(WebSite({
+        uint index = websites.push(Website({
             storageHash: _storageHash,
-            tag: _tag,
             title: _title,
-            description: _description
-        }));
+            description: _description,
+            owner: msg.sender
+        })) -1;
 
-        emit addWebSiteEvent(_tag);
+        hashToIndex[_storageHash] = index;
+
+        for(uint i = 0; i < _tags.length; i++){
+            tagToIndex[_tags[i]].push(index);
+        }
+
+        emit addWebSiteEvent(_tags);
     }
 
-    function  getWebSite(string memory _tag) 
+    function getWebSite(string[] memory _tags) 
         public 
         view 
         returns(string memory website){
       
-        WebSite[] storage webSites = websites[_tag];
+        string memory result;
 
-        string memory result = strConcat("teste1","teste2","teste3","teste4");
+        for(uint a = 0; a < _tags.length; a++){
 
-        return (result);
+            uint[] memory index = tagToIndex[_tags[a]];
+
+            for(uint i = 0; i < index.length; i++){
+                 result = concat(result, websites[i].storageHash, websites[i].title, websites[i].description); 
+            }
+        }
+
+        return result;
     }   
 
-    function strConcat(string memory _a, string memory _b, string memory _c, string memory _d) private pure returns (string memory){
+    function getByHash(string memory storageHash) public view returns (string memory) {
+        uint index = hashToIndex[storageHash];
+        Website memory website = websites[index];
+        return website.title;
+    }
+
+    function getByIndex(uint index) public view returns (string memory) {
+        Website memory website = websites[index];
+        return website.title;
+    }
+
+   function concat(string memory _a, string memory _b, string memory _c, string memory _d) public pure returns (string memory){
         bytes memory _ba = bytes(_a);
         bytes memory _bb = bytes(_b);
         bytes memory _bc = bytes(_c);
         bytes memory _bd = bytes(_d);
         bytes memory _v = bytes(",");
 
-        string memory abcd = new string(_ba.length + _v.length + _bb.length + _v.length + _bc.length  + _v.length + _bd.length);
+        string memory abcd = new string(_ba.length + _v.length + _bb.length + _v.length + _bc.length  + _v.length + _bd.length);   
+
         bytes memory babcd = bytes(abcd);
         uint k = 0;
         for (uint i = 0; i < _ba.length; i++) babcd[k++] = _ba[i];   
@@ -58,7 +87,21 @@ contract Indexer{
         babcd[k++] = _v[0];
         for (uint i = 0; i < _bc.length; i++) babcd[k++] = _bc[i];
         babcd[k++] = _v[0];
-        for (uint i = 0; i < _bd.length; i++) babcd[k++] = _bd[i];
+        for (uint i = 0; i < _bd.length; i++) babcd[k++] = _bd[i];      
         return string(babcd);
+    }    
+    
+     function getStringLength(string memory _text) private pure returns(uint){
+        bytes memory btext = bytes(_text);
+        return btext.length;
     }
+
+    
+    function getSlice(uint256 _begin, uint256 _end, string memory _text) private pure returns (string memory) {
+        bytes memory a = new bytes(_end-_begin+1);
+        for(uint i=0;i<=_end-_begin;i++){
+            a[i] = bytes(_text)[i+_begin-1];
+        }
+        return string(a);    
+    }   
 }
